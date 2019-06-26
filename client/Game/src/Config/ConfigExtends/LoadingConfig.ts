4d6f87b22448e9963cc2519c834694a3d85533e7
zengfeng75 <zengfeng75@gmail.com>
2019-06-26 19:38:47 +0800

    no message

---
+++ client/Game/src/Config/ConfigExtends/LoadingConfig.ts
@@ -0,0 +1,11 @@
+/////////////////////////////////////
+// ExportXlsx生成
+// http://blog.ihaiu.com/tool-ExportXlsx
+/////////////////////////////////////
+
+import LoadingConfigStruct from "../ConfigStructs/LoadingConfigStruct";
+
+export default class LoadingConfig extends LoadingConfigStruct
+{
+	
+}
---
+++ client/Game/src/Config/ReaderExtends/LoadingConfigReader.ts
@@ -0,0 +1,11 @@
+/////////////////////////////////////
+// ExportXlsx生成
+// http://blog.ihaiu.com/tool-ExportXlsx
+/////////////////////////////////////
+
+
+import LoadingConfigReaderStruct from "../ReaderStructs/LoadingConfigReaderStruct";
+
+export default class LoadingConfigReader extends LoadingConfigReaderStruct
+{
+}
